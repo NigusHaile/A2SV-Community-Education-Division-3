9eec5273d5a13143815798892bb75d66b36d7484
NigusHaile <167780709+NigusHaile@users.noreply.github.com>
2024-08-26 22:17:17 +0300

    Create 2165. Smallest Value of the Rearranged Number.py

---
+++ 2165. Smallest Value of the Rearranged Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestNumber(self, num: int) -> int:
+        if num==0:
+            return 0
+        
+        if num>0:
+            digits=sorted(str(num))
+            for i in range(len(digits)):
+                if digits[i]!='0':
+                    digits[0], digits[i]=digits[i], digits[0]
+                    break
+            return int(''.join(digits))
+        else:
+            digits=sorted(str(-num), reverse=True)
+            return -int(''.join(digits))
+
+        
